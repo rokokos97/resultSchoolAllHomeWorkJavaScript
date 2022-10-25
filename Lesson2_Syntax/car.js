751356dccb760297c6d5317704c68e3170db8aa3
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-10-25 17:19:48 +0200

    first lesson Hello World is complete

---
+++ Lesson2_Syntax/car.js
@@ -0,0 +1,21 @@
+const carName = "Audi";
+let maxCarSpeed = "200 km/h";
+let carOwner = "Beatrice";
+console.log("carName: ", carName);
+console.log("maxCarSpeed: ", maxCarSpeed);
+console.log("carOwner: ", carOwner);
+
+carOwner = "Rostyslav";
+alert(`The car ${carName} change owner. Now it is ${carOwner}`);
+maxCarSpeed = "210km/h";
+alert(`The car ${carName} change max speed. Now it is ${maxCarSpeed}`);
+
+carOwner = "Natasha";
+alert(`The car ${carName} change owner. Now it is ${carOwner}`);
+maxCarSpeed = "180km/h";
+alert(`The car ${carName} change max speed. Now it is ${maxCarSpeed}`);
+
+carOwner = "Mateo";
+alert(`The car ${carName} change owner. Now it is ${carOwner}`);
+maxCarSpeed = "150km/h";
+alert(`The car ${carName} change max speed. Now it is ${maxCarSpeed}`);
---
+++ Lesson2_Syntax/index.js
@@ -0,0 +1,16 @@
+// “JavaScript код с созданием переменных”
+/*
+Author:
+Rostyslav Lisovyi
+ */
+const myName = "Rostyslav";
+const mySurname = "Lisovyi";
+let myFavoriteDrink = "Coffee";
+let myFavoriteAnimal = "Dog";
+let myFavoriteProgrammingLanguage = "Javascript";
+console.log("myName is", myName);
+console.log("mySurname is", mySurname);
+console.log("myFavoriteDrink is", myFavoriteDrink);
+console.log("myFavoriteAnimal is", myFavoriteAnimal);
+console.log("myFavoriteProgrammingLanguage is", myFavoriteProgrammingLanguage);
+

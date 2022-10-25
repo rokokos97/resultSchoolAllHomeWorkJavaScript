bd60907772338279191e7f4b6b621d5b8b37e0f7
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-10-25 17:19:48 +0200

    first lesson Hello World is complete

---
+++ Lesson1_HelloWorld/prompt/index.js
@@ -0,0 +1,6 @@
+const firstQuestion = prompt("Сколько будет 2 + 2?");
+alert("Ответ: если вы ответили 4, то вы правы")
+const secondQuestion = prompt("Чему равен корень из 9?");
+alert("Ответ: если вы ответили 3, то вы правы")
+const thirdQuestion = prompt("Сколько будет 2 в 5-й степени?");
+alert("Ответ: если вы ответили 32, то вы правы")

1f30f885b638ee4d8c73d192b543a22aef193df5
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-10-26 11:41:31 +0200

    lesson 3

---
+++ Lesson2_Syntax/const.js
@@ -0,0 +1,13 @@
+const question1 = "Which country is smallest population in the world?";
+const question2 = "Which country is biggest population in the world?";
+const question3 = "Which continent is the biggest in world?";
+const answer1 = "China";
+const answer2 = "Asia";
+const answer3 = "The Vatican City";
+
+console.log(question1);
+console.log(answer3);
+console.log(question2);
+console.log(answer1);
+console.log(question3);
+console.log(answer2);

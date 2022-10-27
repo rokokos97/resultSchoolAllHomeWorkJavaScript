753c9072932ae5e80b9d051ed2806112d78cffbb
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-10-27 21:35:01 +0200

    lesson 6. Extra 6. Exercise2 complete

--- Lesson_6_Javascript_base/extra_5/extra_6/1/index.js
+++ Lesson_6_Javascript_base/extra_5/extra_6/1/index.js
@@ -1,17 +1,27 @@
-const clientName = "Igor";
-let clientSpentForAllTime = 110;
-let clientSpentToday = 25;
-let discount;
-if (100 < clientSpentForAllTime && clientSpentForAllTime < 300) {
-  discount = 10;
-} else if (300 < clientSpentForAllTime && clientSpentForAllTime < 500) {
-  discount = 20;
-} else if (500 < clientSpentForAllTime) {
-  discount = 30;
-}
-alert(`Вам предоставляется скидка в ${discount}%!`);
-clientSpentToday = clientSpentToday - clientSpentToday * (discount / 100);
-clientSpentForAllTime += clientSpentToday;
-alert(
-  `Спасибо, ${clientName}! К оплате ${clientSpentToday}$. За все время в нашем ресторане вы потратили ${clientSpentForAllTime}$.`
+const clientName = prompt("Customer name").trim();
+let clientSpentForAllTime = Number(
+  prompt("How mach cash customer spend in restaurant for all time").trim()
 );
+let clientSpentToday = Number(
+  prompt("How mach cash customer spend today").trim()
+);
+if (!isNaN(clientSpentForAllTime) || !isNaN(clientSpentForAllTime)) {
+  let discount;
+  if (100 < clientSpentForAllTime && clientSpentForAllTime < 300) {
+    discount = 10;
+  } else if (300 < clientSpentForAllTime && clientSpentForAllTime < 500) {
+    discount = 20;
+  } else if (500 < clientSpentForAllTime) {
+    discount = 30;
+  }
+  alert(`Вам предоставляется скидка в ${discount}%!`);
+  clientSpentToday = clientSpentToday - clientSpentToday * (discount / 100);
+  clientSpentForAllTime += clientSpentToday;
+  alert(
+    `Спасибо, ${clientName}! К оплате ${clientSpentToday}$. За все время в нашем ресторане вы потратили ${clientSpentForAllTime}$.`
+  );
+} else {
+  alert(
+    "Сумма, которую клиент потратил за все время и которую потратил сегодня, должна быть числом! Перезагрузите страницу, чтобы повторить попытку."
+  );
+}

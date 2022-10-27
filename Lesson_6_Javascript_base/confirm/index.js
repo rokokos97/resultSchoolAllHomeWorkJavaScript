f5d4a5b8686940750f1335d503d8566517f4e1ef
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-10-27 18:53:35 +0200

    lesson 6 task 3 complete

---
+++ Lesson_6_Javascript_base/confirm/index.js
@@ -0,0 +1,9 @@
+confirm("JavaScript появился в 1995 году?")
+  ? alert("Верно")
+  : alert("JavaScript появился в 1995 году");
+confirm("Спецификация JavaScript называется ECMAScript?")
+  ? alert("Верно")
+  : alert("Неверно! Спецификация JavaScript называется ECMAScript?");
+confirm("JavaScript был создан за 1 месяц?")
+  ? alert("Неверно! JavaScript был создан за 10 дней?")
+  : alert("Верно! JavaScript был создан за 10 дней?");

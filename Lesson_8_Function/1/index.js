f8e223c1f412969b74bfb674c81498e27059f62c
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-10-29 15:09:59 +0200

    lesson 8. Exercise 1 complete

---
+++ Lesson_8_Function/1/index.js
@@ -0,0 +1,15 @@
+const getName1 = function (name) {
+  return `Имя равно ${name}`;
+};
+
+function getName2(name) {
+  return `Имя равно ${name}`;
+}
+
+getName3 = (name) => {
+  return `Имя равно ${name}`;
+};
+
+console.log(getName1("Ros"));
+console.log(getName2("Natasha"));
+console.log(getName1("Nastia"));

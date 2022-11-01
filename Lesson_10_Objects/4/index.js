e56dfc136d5145c06802117728e331b23dbad9e7
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-11-01 21:05:35 +0100

    Lesson_10 Objects Ex 4 complete

---
+++ Lesson_10_Objects/4/index.js
@@ -0,0 +1,22 @@
+giveJobToStudent = (student, jobName) => {
+  alert(
+    `Поздравляем! У студента ${student.fullName} появилась новая работа! Теперь он ${jobName}`
+  );
+  return { ...student, job: jobName };
+};
+const student = {
+  fullName: "Максим",
+  experienceInMonths: 12,
+  stack: ["HTML", "CSS", "JavaScript", "React"],
+};
+
+const updatedStudent = giveJobToStudent(student, "веб-разработчик");
+console.log(updatedStudent);
+/*
+updatedStudent = {
+    fullName: 'Максим',
+    experienceInMonths: 12,
+    stack: ['HTML', 'CSS', 'JavaScript', 'React'],
+    job: 'веб-разработчик',
+}
+*/
